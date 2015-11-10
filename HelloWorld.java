1b402dcf26da2b8a4209af37bdf0b6eeab240043
kelvey <daniel.mckelvey.1@gmail.com>
2015-11-10 15:12:06 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Grupp 49, Daniel McKelvey, Anton Sandern{a:}s
+// Emacs 
+
+ public class HelloWorld {
+
+     public static void  main(String[] args) {
+
+        System.out.println("HelloWorld");
+    }
+}
